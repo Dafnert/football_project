f5f8457462c464650b783d7756926c98251e9796
Adrivzq <adristucom@gmail.com>
2025-09-17 16:42:03 +0200

    The #21 create a new variable

--- src/worldcup/Player.php
+++ src/worldcup/Player.php
@@ -4,13 +4,25 @@ namespace WorldCup;
  class Player extends Person{
 
     private $position;
+    private $name;
+
+    //getter
+
+public function getname() {
+    return $this->name;
+}
+    //setter
+
+    public function setName($name){
+    $this->name = $name;
+    }
 
     //getter
      public function getposition(){
         return $this->position;
 }
 
-    //getter
+    //setter
 
      public function setposition($position): void {
         $this->position = $position;
